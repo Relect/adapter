aceeabc949a1d827932f1a84867b666265194e04
Gennadiy <relect@bk.ru>
2024-10-30 12:45:16 +0300

    first commit

---
+++ src/test/java/com/hawkingbros/adapter/AdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hawkingbros.adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
